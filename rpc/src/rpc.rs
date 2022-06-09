795eee6b7aa7e421d179ae65a9fe35e52d76da81
karneges <karneges2012@gmail.com>
2022-06-09 23:21:23 +0300

    new handler

--- rpc/src/rpc.rs
+++ rpc/src/rpc.rs
@@ -3427,20 +3427,20 @@ pub mod rpc_full {
         ) -> BoxFuture<Result<Option<EncodedConfirmedTransactionWithStatusMeta>>>;
 
         #[rpc(meta, name = "getSignaturesForAddress")]
-        fn get_signatures_for_address(
-            &self,
-            meta: Self::Metadata,
-            address: String,
-            config: Option<RpcSignaturesForAddressConfig>,
-        ) -> BoxFuture<Result<Vec<RpcConfirmedTransactionStatusWithSignature>>>;
-
-        #[rpc(meta, name = "getTransactionsForAddress")]
         fn get_transaction_for_address(
             &self,
             meta: Self::Metadata,
             address: String,
             config: Option<RpcTransactionsForAddressConfig>,
         ) -> BoxFuture<Vec<Option<EncodedConfirmedTransactionWithStatusMeta>>>;
+
+        // #[rpc(meta, name = "getTransactionsForAddress")]
+        // fn get_transaction_for_address(
+        //     &self,
+        //     meta: Self::Metadata,
+        //     address: String,
+        //     config: Option<RpcTransactionsForAddressConfig>,
+        // ) -> BoxFuture<Vec<Option<EncodedConfirmedTransactionWithStatusMeta>>>;
 
         #[rpc(meta, name = "getFirstAvailableBlock")]
         fn get_first_available_block(&self, meta: Self::Metadata) -> BoxFuture<Result<Slot>>;
